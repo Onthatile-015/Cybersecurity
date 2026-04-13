fa99a97258b8a64b0a9c931ea5a8c51e352e3061
Onthatile-015 <ST10486338@rcconnect.edu.za>
2026-04-13 16:30:29 +0200

    Add project files.

---
+++ PROG/VoicePlayer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROG
+{
+    internal class VoicePlayer
+    {
+        //Added a method to play the greeting sound when the program starts.
+        public static void PlayGreeting()
+        {
+            try
+            {
+                SoundPlayer player = new SoundPlayer("greeting.wav");
+                player.PlaySync();
+            }
+            catch
+            {
+                Console.WriteLine("Voice greeting could not be played.");
+            }
+        }
+    }
+
+
+
+}
+
